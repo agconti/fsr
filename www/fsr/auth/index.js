ff80b5ee3ef546f094611332404ae3870f8d3bb9
agconti <agc11d@gmail.com>
2015-08-15 18:34:53 -0400

    feat(auth/index): setup with interceptor and auth service

--- www/fsr/auth/index.js
+++ www/fsr/auth/index.js
@@ -6,21 +6,44 @@
  * App level configuration
  * @constructor
  */
-function config ($stateProvider, $urlRouterProvider) {
+function config ($httpProvider, $stateProvider, $urlRouterProvider) {
+  $httpProvider.interceptors.push('authInterceptor')
+
   $stateProvider
     .state('auth', {
       url: '/auth'
     , templateUrl: './fsr/auth/index.html'
+    , controller: "AuthController"
+    , controllerAs: "auth"
     })
 
-    $urlRouterProvider.when('/auth/callback/', ['$location', '$state', 'store', function ($location, $state, store) {
+    $urlRouterProvider.when('/auth/callback/', ['$location', '$state', 'authFactory', function ($location, $state, authFactory) {
       var access_token = $location.hash().split("=")[1]
-      store.set('token', access_token)
+      authFactory.saveToken(access_token)
       $state.go('auth')
   }])
 }
 
-angular.module('auth', ['angular-storage'])
-.config(['$stateProvider', '$urlRouterProvider', config])
+/**
+ * Initializes the auth module. Forces redirect to login when not authed.
+ * @constructor
+ * @param {object} $rootScope
+ * @param {object} $state
+ * @param {object} authFactory
+ */
+function Initialize ($rootScope, $state, authFactory) {
+  $rootScope.$on('$stateChangeStart', function(e, toState) {
+    if (toState.name === 'auth') { return false }
+
+    if (!authFactory.isLoggedIn()) {
+      e.preventDefault()
+      $state.go('auth')
+    }
+  })
+}
+
+angular.module('auth', ['ui.router', 'angular-storage'])
+.config(['$httpProvider', '$stateProvider', '$urlRouterProvider', config])
+.run(['$rootScope', '$state', 'authFactory', Initialize])
 
 })()
