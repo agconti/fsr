c64bfb1fd63004f0272f58be6233995c0512aac2
agconti <agc11d@gmail.com>
2015-08-15 17:04:54 -0400

    feat(auth/index): auth with fs working

--- www/fsr/auth/index.js
+++ www/fsr/auth/index.js
@@ -1,7 +1,6 @@
 ;(function(){
 'use strict'
 
-var url = 'https://foursquare.com/oauth2/authenticate?client_id=YMV0WS2FQOEO30MCSL3043EXUSFPPSNCCXKLY22QLSSV3I04&response_type=token&redirect_uri=http://localhost:3000/auth/callback/'
 
 /**
  * App level configuration
@@ -12,41 +11,15 @@ function config ($stateProvider, $urlRouterProvider) {
     .state('auth', {
       url: '/auth'
     , templateUrl: './fsr/auth/index.html'
-    , controller: ['$http', '$scope', function($http, $scope){
-        $scope.login = function(){
-          console.log('calling')
-          $http.jsonp(url)
-          .success(function(data){
-            console.log(done)
-            console.log(data)
-          })
-          .error(function(err){
-            console.log('error')
-            console.log(err)
-          })
-        }
-      }]
     })
-    // http://localhost:3000/auth/#access_token=JSBDSJ4XWGSAKTBYVCCR0APMD0YU322PEYJEJ2CBY0EXUUQD
-    .state('auth.callback', {
-      url: '/callback/#access_token=:token'
-    , controller: ['$http','$scope', '$stateParams', function ($http, $scope, $stateParams) {
 
-          console.log('Yoo')
-          console.log($stateParams)
-          alert($stateParams)
-      }]
-    })
+    $urlRouterProvider.when('/auth/callback/', ['$location', '$state', function ($location, $state) {
+      var access_token = $location.hash().split("=")[1]
+      $state.go('auth')
+  }])
 }
 
 angular.module('auth', [])
 .config(['$stateProvider', '$urlRouterProvider', config])
-.run(['$rootScope', function Initialize ($rootScope) {
-  $rootScope.$on('$stateChangeStart', function(e, toState) {
-    console.log(toState, e)
-  })
-}])
-
-
 
 })()
