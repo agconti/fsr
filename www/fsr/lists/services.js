3038071395dbc8fbf6ee9cfa833f71da558424e6
agconti <agc11d@gmail.com>
2015-08-16 16:33:43 -0400

    feat(lists/services): allowed  randomly selected venue
    feat(config): added cordova whitelist plugin
    
    refactor(lists/services): reduced function complexity
    
    fix(lists/services): removed unused vars
    
    style(lists/services): updated indentation

--- www/fsr/lists/services.js
+++ www/fsr/lists/services.js
@@ -2,14 +2,29 @@
 'use strict'
 
 
+
 /**
- * A custom configuration of Restangular for extracting lists from FS.
+ * A custom configuration of Restangular for extracting list items from FS.
  * @constructor
- * @param {object} FS_CONFIG -- foursquare
- * @param {object} store -- from angular-storage
- * @returns {object} ListsFactory
+ * @param {object} Restangular
+ * @returns {object} ListsRestangular
  */
 function ListsRestangular (Restangular) {
+  return Restangular.withConfig(function(RestangularConfigurer){
+    RestangularConfigurer.setResponseExtractor(function(data) {
+      return data.list.listItems.items
+    })
+  })
+}
+
+
+/**
+ * A custom configuration of Restangular for extracting users lists from FS.
+ * @constructor
+ * @param {object} Restangular
+ * @returns {object} UserListsRestangular
+ */
+function UserListsRestangular (Restangular) {
   return Restangular.withConfig(function(RestangularConfigurer){
     RestangularConfigurer.setResponseExtractor(function(data) {
       return data.lists.groups
@@ -21,31 +36,67 @@ function ListsRestangular (Restangular) {
 /**
  * A factory that manages lists from FS.
  * @constructor
- * @param {object} FS_CONFIG -- foursquare
- * @param {object} store -- from angular-storage
+ * @param {object} ListsRestangular
+ * @param {object} UserListsRestangular
  * @returns {object} ListsFactory
  */
-function ListsFactory (FS_CONFIG, store, ListsRestangular) {
-  var meUrl = '/users/self'
-    , myListsUrl = meUrl + '/lists'
-    , myLists = ListsRestangular.all(myListsUrl)
+function ListsFactory (ListsRestangular, UserListsRestangular) {
+  var listUrl = 'lists'
+    , listsResource = ListsRestangular.all(listUrl)
 
-    , venues
+    , myUrl = 'users/self'
+    , myListsUrl = myUrl + '/' + listUrl
+    , myListsResource = UserListsRestangular.all(myListsUrl)
 
-  function getMyLists () {
-    venues = myLists.getList().$object
-    return venues
+  function getMyListsGroups () {
+    return myListsResource.getList().$object
   }
+
+  function getRandomItem(items){
+    return items[ Math.floor(Math.random() * items.length ) ]
+  }
+
+  function getRandomListFromGroups(groups){
+    var groupLength = groups.length
+      , i = 0
+      , group = []
+
+    for(; i < groupLength; i++) {
+      group = getRandomItem(groups)
+
+      if (group.items.length) {
+        break
+      }
+
+      groups.slice(groups.indexOf(group), 1)
+    }
+    return group
+  }
+
+  function getRandomVenueFromRandomList (listGroups) {
+      var listGroup = getRandomListFromGroups(listGroups)
+        , list = getRandomItem(listGroup.items)
+
+      return listsResource.one(list.id)
+        .get()
+        .then(getRandomItem)
+  }
+
+  function getVenue () {
+    return myListsResource
+      .getList()
+      .then(getRandomVenueFromRandomList)
+  }
+
   return {
-    'get': getMyLists
+    'getListsGroups': getMyListsGroups
+  , 'get': getVenue
   }
  }
 
 
 angular.module('lists')
+.factory('UserListsRestangular', ['Restangular', UserListsRestangular])
 .factory('ListsRestangular', ['Restangular', ListsRestangular])
-.factory('listsFactory', [ 'FS_CONFIG'
-                         , 'store'
-                         , 'ListsRestangular'
-                         , ListsFactory])
+.factory('listsFactory', [ 'ListsRestangular', 'UserListsRestangular', ListsFactory])
 })()
