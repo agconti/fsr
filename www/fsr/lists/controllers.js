1834888d0bee8f0d68204cf52b1a68cdcfa79363
agconti <agc11d@gmail.com>
2015-08-21 08:20:00 -0400

    feat(lists/index): added basic styling and layout
    feat(lists/index): added map layout
    
    feat(lists/controllers): referesh with resolve wip
    
    feat(lists/controllers): refresh working without resolve
    
    feat(lists/controllers): pull to refresh with pull to refresh
    
    style(lists/controllers): updated formatting

--- www/fsr/lists/controllers.js
+++ www/fsr/lists/controllers.js
@@ -5,17 +5,34 @@
 /**
  * Manages returning a random venue from fs lists
  * @constructor
+ * @param {object} $scope
  * @param {object} listFactory
  */
-function ListController (listsFactory) {
+function ListController ($scope, listsFactory) {
   var ctr = this
-  listsFactory.get().then(function(venue){
-    ctr.venue = venue
-  })
+
+  this.get = function() {
+    listsFactory.get().then(function(venue){
+      ctr.venue = venue
+    }).finally(function() {
+     $scope.$broadcast('scroll.refreshComplete')
+   })
+  }
+
+  // call on initialization. preping for bable
+  this.get()
+
+  this.getImageUrl = function () {
+   var host = ctr.venue.photo.prefix
+     , size =  [828, 628].join("x")
+     , id = ctr.venue.photo.suffix
+
+   return [host, size, id].join("")
+  }
 }
 
 
 
 angular.module('lists')
-.controller('ListController', ['listsFactory', ListController])
+.controller('ListController', ['$scope', 'listsFactory', ListController])
 })()
